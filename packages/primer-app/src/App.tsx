3604403432bf9a2ce5b6557cc3eae0183a860314
Drew Hess <src@drewhess.com>
2021-09-21 01:19:06 +0100

    [primer-app] Add @fontsource Adobe Source fonts.

--- packages/primer-app/src/App.tsx
+++ packages/primer-app/src/App.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import CallToAction from "./components/CallToAction";
 import Logo from "./components/Logo";
 
+// Required to activate local fonts via Fontsource.
+import "@fontsource/source-sans-pro";
+import "@fontsource/source-serif-pro";
+import "@fontsource/source-code-pro";
+
 function App() {
   return (
     <div className="container flex overflow-hidden flex-col justify-center items-center w-screen h-screen bg-gray-800">
--- packages/primer-app/tailwind.config.js
+++ packages/primer-app/tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+      serif: ["Source Serif Pro", ...defaultTheme.fontFamily.serif],
+      mono: ["Source Code Pro", ...defaultTheme.fontFamily.mono],
+    },
     extend: {
       animation: {
         "spin-slow": "spin 10s linear infinite",
