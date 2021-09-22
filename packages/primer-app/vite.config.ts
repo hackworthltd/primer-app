4a6d17c89b19fa63f8372b9643dc9a4a3abff012
Drew Hess <src@drewhess.com>
2021-09-23 00:39:13 +0100

    [primer-app] Add vite-plugin-checker plugin.
    We configure it to run both `tsc` and `eslint` for dev and production
    builds. In dev mode, it runs them on demand when files change.
    
    Note that this plugin obviates @nabla/vite-plugin-eslint. It also
    means we can remove the `tsc &&` command from the "build" target.

--- packages/primer-app/vite.config.ts
+++ packages/primer-app/vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import svgr from "@honkhonk/vite-plugin-svgr";
-import eslintPlugin from "@nabla/vite-plugin-eslint";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslintPlugin({ formatter: "stylish" }), reactRefresh(), svgr()],
+  plugins: [
+    checker({
+      typescript: true,
+      eslint: { files: ["./src"], extensions: [".ts", ".tsx"] },
+    }),
+    reactRefresh(),
+    svgr(),
+  ],
 
   // Don't write to node_modules, in case someday we can get it from Nix.
   cacheDir: ".vite",
