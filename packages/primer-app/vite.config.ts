800133ea8994b8c4000e69fb5086abf90c05d78f
Drew Hess <src@drewhess.com>
2021-09-22 22:38:44 +0100

    [primer-app] Add @nabla/vite-plugin-eslint.
    Note that this just runs during dev mode, and provides real-time
    `eslint` output when you make changes. It has no effect on other
    modes.

--- packages/primer-app/vite.config.ts
+++ packages/primer-app/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import svgr from "@honkhonk/vite-plugin-svgr";
+import eslintPlugin from "@nabla/vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), svgr()],
+  plugins: [eslintPlugin({ formatter: "stylish" }), reactRefresh(), svgr()],
 
   // Don't write to node_modules, in case someday we can get it from Nix.
   cacheDir: ".vite",
