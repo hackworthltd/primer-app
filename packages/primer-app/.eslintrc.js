56f94d793e8df1c4ca41a65a2a07fc2c3d966576
Drew Hess <src@drewhess.com>
2021-09-27 14:57:57 +0100

    Add primer-lib package to the workspace.
    primer-lib can contain all the common settings used across the
    workspace. For now, it contains the baseline `tsconfig.json` and
    `.eslintrc.js`.
    
    Generally speaking, any configuration that can be extended is fair
    game for primer-lib.

--- packages/primer-app/.eslintrc.js
+++ packages/primer-app/.eslintrc.js
@@ -1,43 +1,5 @@
+const lib = require("@hackworthltd/primer-lib/.eslintrc.js");
+
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-
-  // Generally, you only need to add a plugin to the list if you're
-  // not using its "extends" configuration.
-  plugins: [],
-
-  extends: [
-    "eslint:recommended",
-    "plugin:eslint-comments/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:tailwindcss/recommended",
-    "prettier",
-  ],
-
-  settings: {
-    react: {
-      // Stop eslint-plugin-react from complaining.
-      version: "17",
-    },
-
-    // This keeps the eslint-plugin-import linter in sync with our
-    // TypeScript settings, so we only need to set up path aliases in
-    // tsconfig.json.
-    "import/resolver": {
-      typescript: {},
-    },
-  },
-
-  rules: {
-    // @honkhonk/vite-plugin-svgr uses query parameters, which are not
-    // properly ignored by eslint-plugin-import.
-    //
-    // Note: the `2` here means ignore errors. :\
-    "import/no-unresolved": [2, { ignore: [".svg\\?component"] }],
-  },
+  ...lib,
 };
