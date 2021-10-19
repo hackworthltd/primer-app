ca3096e545053e22d02cc3329d0fa75c5b62a26d
Drew Hess <src@drewhess.com>
2021-10-19 13:24:34 +0100

    Remove leftover Adobe Source font references.

--- packages/primer-app/tailwind.config.js
+++ packages/primer-app/tailwind.config.js
@@ -4,13 +4,7 @@ module.exports = {
   mode: "jit",
   purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-      sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
-      serif: ["Source Serif Pro", ...defaultTheme.fontFamily.serif],
-      mono: ["Source Code Pro", ...defaultTheme.fontFamily.mono],
-    },
-  },
+  theme: {},
   variants: {
     extend: {},
   },
