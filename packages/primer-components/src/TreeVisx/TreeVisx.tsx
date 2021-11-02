c9a27a4e71c4dca62193ae3a43c6f5a29c55fbdc
Ben Price <ben@hackworthltd.com>
2021-11-02 17:12:00 +0000

    REWORD: options for link style; LINT problems
    Lint complains about
    primer-app/packages/primer-components/src/TreeVisx/TreeVisx.tsx
      90:29  warning  Unexpected any. Specify a different type
         @typescript-eslint/no-explicit-any
    However, I don't know how to type it without using 'any' (see comments
    in code).
    
    An upstream example is
    https://airbnb.io/visx/linktypes
    or in more detail
    https://codesandbox.io/s/github/airbnb/visx/tree/master/packages/visx-demo/src/sandboxes/visx-linktypes?file=/getLinkComponent.ts
    which uses 'any'
    
    Perhaps we can just turn off the warning for this occurrence?

--- packages/primer-components/src/TreeVisx/TreeVisx.tsx
+++ packages/primer-components/src/TreeVisx/TreeVisx.tsx
@@ -2,21 +2,35 @@ import "@/index.css";
 import { hierarchy, Tree } from "@visx/hierarchy";
 import { HierarchyPointNode } from "@visx/hierarchy/lib/types";
 import { Group } from "@visx/group";
-import { LinkVerticalLine } from "@visx/shape";
+import {
+  LinkVertical,
+  LinkVerticalLine,
+  LinkVerticalCurve,
+  LinkVerticalStep,
+} from "@visx/shape";
 
 import { Tree as TreeI } from "@hackworthltd/primer-types";
+
+export type LinkType = "line" | "step" | "curve" | "diagonal";
 
 export interface TreeVisxI {
   tree: TreeI;
   width: number;
   height: number;
+  linkType: LinkType;
 }
 
-export const TreeVisx = ({ width, height, tree }: TreeVisxI): JSX.Element => {
+export const TreeVisx = ({
+  width,
+  height,
+  tree,
+  linkType,
+}: TreeVisxI): JSX.Element => {
   const mx = width * 0.1;
   const my = height * 0.1;
   const w = width - 2 * mx;
   const h = height - 2 * my;
+  //const LL = getLink(linkType)
   return (
     <svg width={width} height={height}>
       <rect width={width} height={height} fill="#c33c55" />
@@ -31,12 +45,18 @@ export const TreeVisx = ({ width, height, tree }: TreeVisxI): JSX.Element => {
               {tree.descendants().map((node, i) => (
                 <Node key={i} node={node} />
               ))}
+              {/*tree.links().map((link,i) => <LL 
+                 key={i} data={link}
+                 stroke="black" strokeWidth="1" fill="none"
+                  />)*/}
               {tree.links().map((link, i) => (
-                <LinkVerticalLine
+                <Link
+                  linkType={linkType}
                   key={i}
                   data={link}
                   stroke="black"
                   strokeWidth="1"
+                  fill="none"
                 />
               ))}
             </>
@@ -51,4 +71,37 @@ type HierarchyNode = HierarchyPointNode<TreeI>;
 
 function Node({ node }: { node: HierarchyNode }): JSX.Element {
   return <circle cx={node.x} cy={node.y} r="5" />;
+}
+
+/*
+// get an error about return type is too complex to represent!
+function getLink(linkType : LinkType) : typeof LinkVerticalLine {
+  if (linkType === "line") {
+    return LinkVerticalLine
+  }
+  return LinkVerticalStep
+}
+*/
+
+// I don't see how to type this nicely
+function Link({
+  linkType,
+  ...args
+}: { linkType: LinkType } & any): JSX.Element {
+  // I think the type "should" be the following, but it is not exported from visx, so I cannot write this (iiuc)
+  // (This is from node_modules/@visx/shape/lib/shapes/link/line/LinkVerticalLine.d.ts)
+  //}: { linkType: LinkType } & AddSVGProps<LinkVerticalLineProps<Link, Node>, SVGPathElement>): JSX.Element {
+  // How about being fancy? This fails, but I am not sure I am doing it right...
+  //}: { linkType: LinkType } & Parameters<typeof LinkVerticalLine>): JSX.Element {
+  if (linkType === "line") {
+    return <LinkVerticalLine {...args} />;
+  }
+  if (linkType === "curve") {
+    return <LinkVerticalCurve {...args} />;
+  }
+  if (linkType === "step") {
+    return <LinkVerticalStep {...args} />;
+  }
+  // otherwise, (linkType === "diagonal") {
+  return <LinkVertical {...args} />;
 }
