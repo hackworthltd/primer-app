50e670f2a9db6b44a7fd65839b78718579cd7466
Drew Hess <src@drewhess.com>
2021-11-15 12:45:12 +0000

    feat: Add an `ActionButton`.
    This is styled similarly to action buttons in Vonnegut, but
    incorporating our most recent UI design changes.

---
+++ packages/primer-components/src/ActionButton/ActionButton.tsx
@@ -0,0 +1,46 @@
+import "@/index.css";
+
+import classNames from "classnames";
+
+export type Appearance = "primary" | "warning";
+
+export interface ActionButtonProps {
+  /**
+   * Pre-defined button appearances.
+   *
+   * @type {Appearance}
+   */
+  appearance: Appearance;
+
+  /**
+   * The button label.
+   *
+   * @type {string}
+   */
+  label: string;
+
+  /**
+   * The action description.
+   *
+   * @type {string}
+   */
+  description: string;
+}
+
+const buttonClasses = (appearance: Appearance) =>
+  classNames({
+    "border-indigo-700 text-indigo-700 hover:bg-indigo-700 hover:text-white focus:ring-indigo-500":
+      appearance === "primary",
+    "border-red-600 text-red-600 hover:bg-red-600 hover:text-white focus:ring-red-500":
+      appearance === "warning",
+    "inline-flex items-center w-full px-6 py-3 text-base rounded border font-medium shadow-sm bg-white focus:outline-none focus:ring-2 focus:ring-offset-2": true,
+  });
+
+export const ActionButton = (p: ActionButtonProps): JSX.Element => (
+  <button type="button" className={buttonClasses(p.appearance)}>
+    <div className="mr-8 -ml-1" aria-hidden="true">
+      {p.label}
+    </div>
+    {p.description}
+  </button>
+);
--- packages/primer-components/src/index.ts
+++ packages/primer-components/src/index.ts
@@ -1,3 +1,4 @@
+export { default as ActionButton } from "@/ActionButton";
 export { default as Avatar } from "@/Avatar";
 export { default as BinaryTreePlaceholder } from "@/BinaryTreePlaceholder";
 export { default as CallToAction } from "@/CallToAction";
