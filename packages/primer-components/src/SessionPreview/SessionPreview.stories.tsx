0272786e11c11020c5e7e664e862785d9124c784
Drew Hess <src@drewhess.com>
2021-11-24 13:09:21 +0000

    Move remaining example sessions into the examples module.

--- packages/primer-components/src/SessionPreview/SessionPreview.stories.tsx
+++ packages/primer-components/src/SessionPreview/SessionPreview.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { v4 as uuidv4 } from "uuid";
+import * as examples from "@/examples/sessions";
 
 import { SessionPreview } from "./SessionPreview";
 
@@ -11,57 +11,31 @@ export default {
   },
 } as ComponentMeta<typeof SessionPreview>;
 
-const id1: string = uuidv4();
-const id2: string = uuidv4();
-const id3: string = uuidv4();
-const id4: string = uuidv4();
-const id5: string = uuidv4();
-
 const Template: ComponentStory<typeof SessionPreview> = (args) => (
   <SessionPreview {...args} />
 );
 
 export const English = Template.bind({});
 English.args = {
-  session: {
-    id: id1,
-    name: "Example program",
-    lastModified: new Date("2021-08-15T23:17:50.918Z"),
-  },
+  session: examples.englishSessionMeta,
 };
 
 export const Japanese = Template.bind({});
 Japanese.args = {
-  session: {
-    id: id2,
-    name: "サンプルプログラム",
-    lastModified: new Date("2021-08-07T10:12:03.332Z"),
-  },
+  session: examples.japaneseSessionMeta,
 };
 
 export const SimplifiedChinese = Template.bind({});
 SimplifiedChinese.args = {
-  session: {
-    id: id3,
-    name: "示例程序",
-    lastModified: new Date("2021-07-10T09:00:01.000Z"),
-  },
+  session: examples.simplifiedChineseSessionMeta,
 };
 
 export const Arabic = Template.bind({});
 Arabic.args = {
-  session: {
-    id: id4,
-    name: "برنامج مثال",
-    lastModified: new Date("2021-08-20T03:20:59.999Z"),
-  },
+  session: examples.arabicSessionMeta,
 };
 
 export const Emoji = Template.bind({});
 Emoji.args = {
-  session: {
-    id: id5,
-    name: "😄😂🤣🤗 🦊 🦈",
-    lastModified: new Date("2021-07-23T11:53:13.730Z"),
-  },
+  session: examples.emojiSessionMeta,
 };
--- packages/primer-components/src/examples/sessions.ts
+++ packages/primer-components/src/examples/sessions.ts
@@ -1,36 +1,60 @@
 import { v4 as uuidv4 } from "uuid";
 import type { SessionMeta } from "@hackworthltd/primer-types";
 
-export const exampleSessionsMeta = (): SessionMeta[] => {
-  const id1: string = uuidv4();
-  const id2: string = uuidv4();
-  const id3: string = uuidv4();
-  const id4: string = uuidv4();
-  const id5: string = uuidv4();
+export const englishSessionMeta: SessionMeta = {
+  id: uuidv4(),
+  name: "Example program",
+  lastModified: new Date("2021-08-15T23:17:50.918Z"),
+};
 
+export const japaneseSessionMeta: SessionMeta = {
+  id: uuidv4(),
+  name: "サンプルプログラム",
+  lastModified: new Date("2021-08-07T10:12:03.332Z"),
+};
+
+export const simplifiedChineseSessionMeta: SessionMeta = {
+  id: uuidv4(),
+  name: "示例程序",
+  lastModified: new Date("2021-07-10T09:00:01.000Z"),
+};
+
+export const arabicSessionMeta: SessionMeta = {
+  id: uuidv4(),
+  name: "برنامج مثال",
+  lastModified: new Date("2021-08-20T03:20:59.999Z"),
+};
+
+export const emojiSessionMeta: SessionMeta = {
+  id: uuidv4(),
+  name: "😄😂🤣🤗 🦊 🦈",
+  lastModified: new Date("2021-07-23T11:53:13.730Z"),
+};
+
+export const exampleSessionsMeta = (): SessionMeta[] => {
   return [
+    englishSessionMeta,
+    japaneseSessionMeta,
+    simplifiedChineseSessionMeta,
+    arabicSessionMeta,
+    emojiSessionMeta,
     {
-      id: id1,
+      id: uuidv4(),
       name: "Map test",
       lastModified: new Date("2021-08-15T23:17:50.918Z"),
     },
     {
-      id: id2,
+      id: uuidv4(),
       name: "Just playing around",
       lastModified: new Date("2021-08-07T10:12:03.332Z"),
     },
     {
-      id: id3,
-      name: "😄😂🤣🤗 🦊 🦈",
-      lastModified: new Date("2021-07-23T11:53:13.730Z"),
-    },
-    {
-      id: id4,
+      id: uuidv4(),
       name: "Exercise 2.3(b)",
       lastModified: new Date("2021-07-10T09:00:01.000Z"),
     },
     {
-      id: id5,
+      id: uuidv4(),
       name: "Not yet working",
       lastModified: new Date("2021-08-20T03:20:59.999Z"),
     },
