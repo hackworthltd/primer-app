133bac8871cad792a3b352046546c7e0d1b95d68
Drew Hess <src@drewhess.com>
2021-11-25 11:49:18 +0000

    refactor: Move example account into new examples module.

--- packages/primer-components/src/SessionsPage/SessionsPage.stories.tsx
+++ packages/primer-components/src/SessionsPage/SessionsPage.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import type { Account } from "@hackworthltd/primer-types";
 import { SessionsPage } from "./SessionsPage";
 import { exampleSessionsMeta } from "@/examples/sessions";
+import { exampleAccount } from "@/examples/accounts";
 
 export default {
   title: "Application/Component Library/SessionsPage",
@@ -12,20 +12,12 @@ export default {
   },
 } as ComponentMeta<typeof SessionsPage>;
 
-const account: Account = {
-  name: "Chelsea Hagon",
-  email: "chelseahagon@example.com",
-  avatarStyle: "jdenticon",
-  imageUrl:
-    "https://images.unsplash.com/photo-1573496358961-3c82861ab8f4?ixid=MnwyNjcwMzh8MHwxfGFsbHx8fHx8fHx8fDE2MzQwNTU4MjU&ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
-
 const Template: ComponentStory<typeof SessionsPage> = (args) => (
   <SessionsPage {...args} />
 );
 
 export const Default = Template.bind({});
 Default.args = {
-  account: account,
+  account: exampleAccount,
   sessions: exampleSessionsMeta(),
 };
