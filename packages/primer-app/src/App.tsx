71647f788b5b1bfe1ef9105a746dfd4020f8a7dd
Drew Hess <src@drewhess.com>
2021-12-01 14:22:18 +0000

    fix: primer-app now displays the sessions page.
    This doesn't do anything particularly useful yet (and needs some
    styling fixes), but at least now we can get rid of the placeholder
    CallToAction and Logo components leftover from the starter React
    application.

--- packages/primer-app/src/App.tsx
+++ packages/primer-app/src/App.tsx
@@ -1,11 +1,9 @@
-import { CallToAction, Logo } from "@hackworthltd/primer-components";
+import { exampleSessionsMeta, exampleAccount, SessionsPage } from "@hackworthltd/primer-components";
+
 import "@hackworthltd/primer-components/style.css";
 
 const App = (): JSX.Element => (
-  <div className="container flex overflow-hidden flex-col justify-center items-center w-screen h-screen bg-gray-800">
-    <Logo />
-    <CallToAction />
-  </div>
+  <SessionsPage account={exampleAccount} sessions={exampleSessionsMeta()} />
 );
 
 export default App;
