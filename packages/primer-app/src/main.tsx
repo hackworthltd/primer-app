1df13b95dadd41044638af3374db787b57b6b976
Drew Hess <src@drewhess.com>
2021-12-07 12:11:33 +0000

    feat: Add React Router to the app.
    At the moment, this only configures React Router to use the browser's
    history.

--- packages/primer-app/src/main.tsx
+++ packages/primer-app/src/main.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+const rootElement: HTMLElement | null = document.getElementById("root");
 render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  rootElement
 );
