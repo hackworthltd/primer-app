30db5cfdd18bdbfc90f0f7d64760415bb21583d9
Ben Price <ben@hackworthltd.com>
2021-12-08 13:37:58 +0000

    Add D3 to interactive trees
    Note that the D3 tree is not itself interactive. It is useful to be able
    to see the same tree in multiple renderings, even if they are not all
    fully-featured.
    
    We also add a margin to the svg and change the size of the story
    renders, to more closely match the visx styling.

--- packages/primer-components/src/TreeD3/TreeD3.stories.tsx
+++ packages/primer-components/src/TreeD3/TreeD3.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import TreeD3 from "@/TreeD3";
-import { Tree } from "@hackworthltd/primer-types";
+import { TreeVisxI, LinkType } from "@/TreeVisx";
 import { tree1, tree2, tree3, tree4, tree5 } from "@/examples/trees";
 
 export default {
@@ -12,29 +12,24 @@ export default {
   },
 } as ComponentMeta<typeof TreeD3>;
 
-/* We have this indirection so storybook renders the whole json description of
- * a tree as one control. (As having three separate controls for 'label', 'id'
- * and 'subtrees' is not particularly useful.)
- */
-interface TreeArgs {
-  tree: Tree;
-}
-
-const Template: ComponentStory<(args: TreeArgs) => JSX.Element> = (args) => (
+const Template: ComponentStory<(args: TreeVisxI) => JSX.Element> = (args) => (
   <TreeD3 {...args} />
 );
 
+const line: LinkType = "line";
+const size = { width: 400, height: 300, linkType: line };
+
 export const Tree1 = Template.bind({});
-Tree1.args = { tree: tree1 };
+Tree1.args = { ...size, tree: tree1 };
 
 export const Tree2 = Template.bind({});
-Tree2.args = { tree: tree2 };
+Tree2.args = { ...size, tree: tree2 };
 
 export const Tree3 = Template.bind({});
-Tree3.args = { tree: tree3 };
+Tree3.args = { ...size, tree: tree3 };
 
 export const Tree4 = Template.bind({});
-Tree4.args = { tree: tree4 };
+Tree4.args = { ...size, tree: tree4 };
 
 export const Tree5 = Template.bind({});
-Tree5.args = { tree: tree5 };
+Tree5.args = { ...size, tree: tree5 };
