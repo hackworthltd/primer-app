a57fab48163284c24dc625b5be77627ff370adab
Drew Hess <src@drewhess.com>
2022-01-13 15:41:39 +0000

    fix: The Vite server can now speak to a Primer localhost backend.
    This currently requires that you run `nix run .#run-primer` (or
    equivalent) in a checked-out Primer repo as a separate step. We'll
    make this easier in a future commit.

--- packages/primer-app/vite.config.ts
+++ packages/primer-app/vite.config.ts
@@ -26,6 +26,12 @@ export default defineConfig({
     fs: {
       allow: ["../.."],
     },
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8081",
+        changeOrigin: true
+      }
+    }
   },
 
   build: {
