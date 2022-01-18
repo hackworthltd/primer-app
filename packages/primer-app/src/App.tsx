f3e5cad970048d5db0f8e0279a5a9b718128fbea
Drew Hess <src@drewhess.com>
2022-01-18 14:37:03 +0000

    Baby steps towards a paginated session list.
    Here we just demonstrate that it's possible to implement this via the
    APIs of the various layers. For now we only fetch 1 page and we set
    the default page size to 4 in order to exercise the UI a bit.

--- packages/primer-app/src/App.tsx
+++ packages/primer-app/src/App.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import type { SessionMeta } from "@hackworthltd/primer-types";
 import { exampleAccount, SessionsPage } from "@hackworthltd/primer-components";
-import "@hackworthltd/primer-components/style.css";
 import type { Session } from "@/primer-api";
-import { useGetSessionList } from "@/primer-api";
+import { useGetSessionList, GetSessionListParams } from "@/primer-api";
+
+import "@hackworthltd/primer-components/style.css";
 
 const App = (): JSX.Element => {
-  const { data: page } = useGetSessionList();
+  // NOTE: pagination in our API is 1-indexed.
+  const [page] = useState(1);
 
-  // Backend sessions do not yet have `lastModified`, so we just create one on the fly.
-  const sessions: Session[] = page? page.items : [];
+  // XXX dhess: the default page size of 4 is chosen here just to
+  // exercise the UI a bit. We should almost certainly make the
+  // default value larger, and potentially set it based on the
+  // device's viewport size.
+  const [pageSize] = useState(4);
+
+  const params: GetSessionListParams = { page: page, pageSize: pageSize };
+  const { data } = useGetSessionList(params);
+
+  // Backend sessions do not yet have `lastModified`, so we just
+  // create one on the fly for each retrieved session.
+  const sessions: Session[] = data? data.items : [];
   const sessionsMeta: SessionMeta[] = sessions.map((session: Session) => ({ name: session.name, id: session.id, lastModified: new Date() }));
 
   return <SessionsPage account={exampleAccount} sessions={sessionsMeta} />;
