da34f755bbd8406547b76d60ecaaea168a1ca1fa
Drew Hess <src@drewhess.com>
2022-01-18 13:24:51 +0000

    feat: Basic support for displaying the backend session list.
    There's no error handling yet, nor any support for pagination, but we
    can now display one (default-sized) page of sessions from a
    locally-running backend Primer service.
    
    Note that we have to fudge the `lastModified` date shown for sessions,
    because the backend doesn't support them yet. Therefore there's a bit
    of code here to transform the backend `Session` type to the frontend's
    `SessionMeta` type. (Eventually there should just be the one `Session`
    type.)

--- packages/primer-app/src/App.tsx
+++ packages/primer-app/src/App.tsx
@@ -1,9 +1,17 @@
-import { exampleSessionsMeta, exampleAccount, SessionsPage } from "@hackworthltd/primer-components";
-
+import type { SessionMeta } from "@hackworthltd/primer-types";
+import { exampleAccount, SessionsPage } from "@hackworthltd/primer-components";
 import "@hackworthltd/primer-components/style.css";
+import type { Session } from "@/primer-api";
+import { useGetSessionList } from "@/primer-api";
 
-const App = (): JSX.Element => (
-  <SessionsPage account={exampleAccount} sessions={exampleSessionsMeta()} />
-);
+const App = (): JSX.Element => {
+  const { data: page } = useGetSessionList();
+
+  // Backend sessions do not yet have `lastModified`, so we just create one on the fly.
+  const sessions: Session[] = page? page.items : [];
+  const sessionsMeta: SessionMeta[] = sessions.map((session: Session) => ({ name: session.name, id: session.id, lastModified: new Date() }));
+
+  return <SessionsPage account={exampleAccount} sessions={sessionsMeta} />;
+};
 
 export default App;
--- packages/primer-app/src/main.tsx
+++ packages/primer-app/src/main.tsx
@@ -1,19 +1,24 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
 
 import { NoMatch } from "@hackworthltd/primer-components";
 import App from "./App";
 
+const queryClient = new QueryClient();
 const rootElement: HTMLElement | null = document.getElementById("root");
+
 render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
   rootElement
