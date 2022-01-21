bd5570b71d136a9e0d02c26551d781def0f798cc
Drew Hess <src@drewhess.com>
2022-01-21 14:16:23 +0000

    style: Simplify the sessions nav bar.
    The mobile/responsive design we took from Tailwind UI for this
    component never worked correctly, and made the component quite
    complicated, to boot. Here we simply rip out the mobile design and use
    a uniform design across all breakpoints.
    
    I've also tweaked the component's margins and inter-element padding to
    eliminate some odd behaviors that the previous design exhibited.
    
    This still needs some work, especially at the smallest 2 breakpoints,
    but it's an improvement over the previously (broken) design.

--- packages/primer-components/src/SessionsNavBar/SessionsNavBar.tsx
+++ packages/primer-components/src/SessionsNavBar/SessionsNavBar.tsx
@@ -1,8 +1,7 @@
 import type { MouseEventHandler } from "react";
 import { Fragment } from "react";
 import classNames from "classnames";
-import { Menu, Popover, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Menu, Transition } from "@headlessui/react";
 import type { Account } from "@hackworthltd/primer-types";
 import { Avatar } from "@/Avatar/Avatar";
 import { UIButton } from "@/UIButton/UIButton";
@@ -35,133 +34,65 @@ const accountNavigation = [
 
 export const SessionsNavBar = (p: SessionsNavBarProps): JSX.Element => (
   <>
-    {/* When the mobile menu is open, add `overflow-y-auto` to prevent double scrollbars. */}
-    <Popover
-      as="header"
-      className={({ open }) =>
-        classNames(
-          open ? "fixed inset-0 z-40 overflow-y-auto" : "",
-          "lg:static lg:overflow-y-visible"
-        )
-      }
-    >
-      {({ open }) => (
-        <>
-          <div className="px-4 sm:px-6 lg:px-8 mx-auto max-w-7xl">
-            <div className="flex xl:grid relative xl:grid-cols-12 lg:gap-8 justify-between">
-              <div className="flex">
-                <PrimerBranding size="responsive" />
-              </div>
-              <div className="flex-1 xl:col-span-6 md:px-8 lg:px-0 min-w-0">
-                <div className="flex items-center py-4 px-6 xl:px-0 md:mx-auto lg:mx-0 md:max-w-3xl lg:max-w-none">
-                  <SearchBar
-                    ariaLabel="Search programs"
-                    placeholder="Program name"
-                  />
-                </div>
-              </div>
-              <div className="flex lg:hidden md:absolute md:inset-y-0 md:right-0 items-center">
-                {/* Mobile menu button */}
-                <Popover.Button className="inline-flex justify-center items-center p-2 -mx-2 text-blue-primary hover:text-blue-secondary hover:bg-grey-primary rounded-md focus:ring-2 focus:ring-inset focus:ring-blue-primary focus:outline-none">
-                  <span className="sr-only">Open menu</span>
-                  {open ? (
-                    <XIcon className="block w-6 h-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block w-6 h-6" aria-hidden="true" />
-                  )}
-                </Popover.Button>
-              </div>
-              <div className="hidden lg:flex xl:col-span-4 lg:justify-end lg:items-center">
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative shrink-0 ml-5">
-                  <div>
-                    <Menu.Button className="flex bg-white-primary rounded-full focus:ring-2 focus:ring-blue-primary focus:ring-offset-2 focus:outline-none">
-                      <span className="sr-only">Open account menu</span>
-                      <Avatar
-                        id={p.account.email}
-                        style={p.account.avatarStyle}
-                        imgSrc={p.account.imageUrl}
-                        decoration="plain"
-                        size="responsive"
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 py-1 mt-2 w-48 text-blue-primary bg-white-primary rounded-md ring-1 ring-blue-primary ring-opacity-5 shadow-lg origin-top-right focus:outline-none">
-                      {accountNavigation.map((item) => (
-                        <Menu.Item key={item.name}>
-                          {({ active }) => (
-                            <a
-                              href={item.href}
-                              className={classNames(
-                                active ? "bg-white-primary" : "",
-                                "block py-2 px-4 text-sm text-blue-primary hover:text-white-primary hover:bg-blue-secondary"
-                              )}
-                            >
-                              {item.name}
-                            </a>
-                          )}
-                        </Menu.Item>
-                      ))}
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-                <div className="inline-flex items-center py-2 px-4 ml-4">
-                  <UIButton
-                    size="responsive"
-                    appearance="primary"
-                    text="New program"
-                    onClick={p.onClickNewProgram}
-                  />
-                </div>
-              </div>
-            </div>
+    <div className="static overflow-y-visible flex relative justify-between">
+      <div className="flex py-6">
+        <PrimerBranding size="responsive" />
+      </div>
+      <div className="flex-1 py-6 items-center px-6 min-w-0">
+        <SearchBar ariaLabel="Search programs" placeholder="Program name" />
+      </div>
+      <div className="flex justify-end items-center">
+        {/* Profile dropdown */}
+        <Menu as="div" className="relative shrink-0">
+          <div>
+            <Menu.Button className="flex bg-white-primary rounded-full focus:ring-2 focus:ring-blue-primary focus:ring-offset-2 focus:outline-none">
+              <span className="sr-only">Open account menu</span>
+              <Avatar
+                id={p.account.email}
+                style={p.account.avatarStyle}
+                imgSrc={p.account.imageUrl}
+                decoration="plain"
+                size="responsive"
+              />
+            </Menu.Button>
           </div>
-
-          <Popover.Panel as="nav" className="lg:hidden" aria-label="Global">
-            <div className="pt-4 pb-3 border-t border-grey-primary">
-              <div className="flex items-center px-4 sm:px-6 mx-auto max-w-3xl">
-                <div className="shrink-0">
-                  <Avatar
-                    id={p.account.email}
-                    style={p.account.avatarStyle}
-                    imgSrc={p.account.imageUrl}
-                    decoration="plain"
-                    size="responsive"
-                  />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-blue-primary">
-                    {p.account.name}
-                  </div>
-                  <div className="text-sm font-medium text-blue-primary">
-                    {p.account.email}
-                  </div>
-                </div>
-              </div>
-              <div className="px-2 sm:px-4 mx-auto mt-3 space-y-1 max-w-3xl">
-                {accountNavigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="block py-2 px-3 text-base font-medium text-blue-primary hover:text-white-primary hover:bg-blue-primary rounded-md"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div>
-          </Popover.Panel>
-        </>
-      )}
-    </Popover>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 py-1 mt-2 w-48 text-blue-primary bg-white-primary rounded-md ring-1 ring-blue-primary ring-opacity-5 shadow-lg origin-top-right focus:outline-none">
+              {accountNavigation.map((item) => (
+                <Menu.Item key={item.name}>
+                  {({ active }) => (
+                    <a
+                      href={item.href}
+                      className={classNames(
+                        active ? "bg-white-primary" : "",
+                        "block py-2 px-4 text-sm text-blue-primary hover:text-white-primary hover:bg-blue-secondary"
+                      )}
+                    >
+                      {item.name}
+                    </a>
+                  )}
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Transition>
+        </Menu>
+        <div className="inline-flex items-center py-2 pl-4">
+          <UIButton
+            size="responsive"
+            appearance="primary"
+            text="New program"
+            onClick={p.onClickNewProgram}
+          />
+        </div>
+      </div>
+    </div>
   </>
 );
