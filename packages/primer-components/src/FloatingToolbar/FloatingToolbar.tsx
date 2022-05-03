51ac47184e49623fb56deded3fc948cca0928d07
George Thomas <georgefsthomas@gmail.com>
2022-05-03 17:42:32 +0100

    fix: Disable text selection on floating toolbar component
    This is actually necessary to prevent visual artefacts on Safari when dragging (see https://github.com/react-grid-layout/react-draggable/issues/605).

--- packages/primer-components/src/FloatingToolbar/FloatingToolbar.tsx
+++ packages/primer-components/src/FloatingToolbar/FloatingToolbar.tsx
@@ -58,6 +58,7 @@ export const FloatingToolbar = (p: FloatingToolbarProps): JSX.Element => {
         className={classNames(
           "flex flex-col gap-2 justify-center items-center",
           "text-blue-primary bg-grey-primary rounded shadow-lg",
+          "select-none",
           touchDragging ? "p-5 w-24 -my-1 -mx-2" : "p-4 w-20"
         )}
       >
