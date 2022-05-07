78efe2c6034ebfc2e9d7c8447191e0aa62384489
Drew Hess <src@drewhess.com>
2022-05-07 23:39:47 +0100

    chore: Switch Storybook config to TypeScript.

--- packages/primer-components/.storybook/main.ts
+++ packages/primer-components/.storybook/main.ts
@@ -1,25 +1,22 @@
-const path = require("path");
-const svgr = require("@honkhonk/vite-plugin-svgr").default;
-const tsconfigPaths = require("vite-tsconfig-paths").default;
+import svgr from "@honkhonk/vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+import type { StorybookViteConfig } from "@storybook/builder-vite";
 
-module.exports = {
+const config: StorybookViteConfig = {
+  framework: "@storybook/react",
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   core: {
     builder: "@storybook/builder-vite",
   },
-
-  // storybook-builder-vite doesn't read the project's vite.config.js,
-  // so we have to recreate the important bits here, mainly the
-  // resolvers and the relevant plugins.
-  async viteFinal(config, { configType }) {
+  async viteFinal(config, _) {
     return {
       ...config,
-
       plugins: [...config.plugins, svgr(), tsconfigPaths()],
-
       // Don't write to node_modules, in case someday we can get it from Nix.
       cacheDir: "../.vite",
     };
   },
 };
+
+export default config;
