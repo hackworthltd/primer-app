401eb30feba74904ef1fc5d198d7f21c81d43ddf
Drew Hess <src@drewhess.com>
2022-07-06 11:07:39 +0100

    feat: Proof-of-concept that we can pull in `primer` Nix packages.
    Mainly we want to demonstrate in this commit that we can pull `primer`
    Nix packages into the `primer-app` flake without needing to rebuild
    them from scratch. The reason this is important to demonstrate is that
    `primer` packages take a long time to build, especially if we need to
    build, say, GHC, and therefore we want `primer-app` to take full
    advantage of our `primer` Nix binary cache.
    
    And in fact, in my testing, building the
    `packages.x86_64-linux.primer-service-docker-image` package does pull
    the package from the Nix binary cache, rather than rebuilding it.

--- flake.nix
+++ flake.nix
@@ -17,6 +17,10 @@
     pre-commit-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
     # Fixes aarch64-darwin support.
     pre-commit-hooks-nix.inputs.flake-utils.follows = "flake-utils";
+
+    # Note: don't override any of primer's Nix flake inputs, or else
+    # we won't hit its binary cache.
+    primer.url = github:hackworthltd/primer/860660acfaeefdc3e88595e73a2009c2341123c9;
   };
 
   outputs =
@@ -26,6 +30,7 @@
     , hacknix
     , flake-utils
     , pre-commit-hooks-nix
+    , primer
     , ...
     }@inputs:
     let
@@ -85,6 +90,8 @@
       let
         pkgs = pkgsFor system;
 
+        primerPackages = primer.packages.${system};
+
         pre-commit-hooks =
           let
           in
@@ -114,6 +121,10 @@
           };
       in
       {
+        packages = { } // (pkgs.lib.optionalAttrs (system == "x86_64-linux") {
+          inherit (primerPackages) primer-service-docker-image;
+        });
+
         checks = {
           source-code-checks = pre-commit-hooks;
         };
