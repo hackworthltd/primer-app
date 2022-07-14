dc88b1a73b41821a8e5b1e9dee41554430242dd3
George Thomas <georgefsthomas@gmail.com>
2022-07-14 11:24:53 +0100

    fix: Show an error instead of `null` if `App` has no `sessionId` param
    NB this is an internal error condition which should never actually occur.

--- packages/primer-app/src/App.tsx
+++ packages/primer-app/src/App.tsx
@@ -1,23 +1,30 @@
 // TODO this is very much a placeholder
-import { TreeReactFlow, tree1, tree4 } from "@hackworthltd/primer-components";
+import { TreeReactFlow, tree1, tree4, Error } from "@hackworthltd/primer-components";
 import "@hackworthltd/primer-components/style.css";
 import { useParams } from "react-router-dom";
 
 const App = (): JSX.Element => {
   const params = useParams();
+  const session = params["sessionId"];
 
-  return (
-    <div>
-      {"Session: " + params["sessionId"]}
-      <TreeReactFlow
-        trees={[tree1, tree4]}
-        width={1000}
-        height={800}
-        nodeWidth={100}
-        nodeHeight={40}
-      ></TreeReactFlow>
-    </div>
-  );
+  if (session) {
+    return (
+      <div>
+        {"Session: " + session}
+        <TreeReactFlow
+          trees={[tree1, tree4]}
+          width={1000}
+          height={800}
+          nodeWidth={100}
+          nodeHeight={40}
+        ></TreeReactFlow>
+      </div>
+    );
+  } else {
+    return (
+      <Error string={"No session id parameter: " + JSON.stringify(params)} />
+    );
+  }
 };
 
 export default App;
