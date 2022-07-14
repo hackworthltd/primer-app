1604ab43546d9ecfb528efb8d044779f3211f44e
George Thomas <georgefsthomas@gmail.com>
2022-07-14 12:03:20 +0100

    refactor: Avoid "pyramid of doom"-style error checking in `App.tsx`
    With the previous style, further indentation was needed for each new check. Instead, we now utilise early returns to keep a flatter tree.

--- packages/primer-app/src/App.tsx
+++ packages/primer-app/src/App.tsx
@@ -7,55 +7,50 @@ const App = (): JSX.Element => {
   const params = useParams();
   const session = params["sessionId"];
 
-  if (session) {
-    const queryRes = useGetApiProgram({ session });
-
-    if (queryRes.isSuccess) {
-      const prog = queryRes.data;
-
-      const editableModules = prog.modules.filter((m) => m.editable);
-      if (editableModules.length > 1) {
-        return (
-          <Error
-            string={
-              "Multiple editable modules: " + JSON.stringify(editableModules)
-            }
-          />
-        );
-      }
-      const module = editableModules[0];
-
-      if (module) {
-        const trees = module.defs.flatMap((def) => def.term || []);
-        return (
-          <div>
-            <TreeReactFlow
-              trees={trees}
-              width={1500}
-              height={800}
-              nodeWidth={200}
-              nodeHeight={100}
-            ></TreeReactFlow>
-          </div>
-        );
-      } else {
-        return <Error string="No editable modules" />;
-      }
-    } else {
-      return (
-        <Error
-          string={
-            "Failed to get program from backend: " +
-            JSON.stringify(queryRes.error)
-          }
-        />
-      );
-    }
-  } else {
+  if (!session) {
     return (
       <Error string={"No session id parameter: " + JSON.stringify(params)} />
     );
   }
+  const queryRes = useGetApiProgram({ session });
+
+  if (!queryRes.isSuccess) {
+    return (
+      <Error
+        string={
+          "Failed to get program from backend: " +
+          JSON.stringify(queryRes.error)
+        }
+      />
+    );
+  }
+  const prog = queryRes.data;
+
+  const editableModules = prog.modules.filter((m) => m.editable);
+  if (editableModules.length > 1) {
+    return (
+      <Error
+        string={"Multiple editable modules: " + JSON.stringify(editableModules)}
+      />
+    );
+  }
+  const module = editableModules[0];
+  if (!module) {
+    return <Error string="No editable modules" />;
+  }
+  const trees = module.defs.flatMap((def) => def.term || []);
+
+  return (
+    <div>
+      <TreeReactFlow
+        trees={trees}
+        width={1500}
+        height={800}
+        nodeWidth={200}
+        nodeHeight={100}
+      ></TreeReactFlow>
+    </div>
+  );
 };
 
 export default App;
