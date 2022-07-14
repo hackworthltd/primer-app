3a638b218aaaed3a2e994545f64643b387ae357e
George Thomas <georgefsthomas@gmail.com>
2022-07-14 11:45:14 +0100

    feat: Render a module from the backend
    This replaces the previous hardcoded trees.
    
    Note that we don't yet display a definition's name or type. These will be added in due course, once we've made some design decisions about how to lay out multiple trees.
    
    We also currently require that the session contains precisely one editable module, throwing errors otherwise. We will some day relax this decision, once UX issues have been worked out.

--- packages/primer-app/src/App.tsx
+++ packages/primer-app/src/App.tsx
@@ -1,25 +1,56 @@
-// TODO this is very much a placeholder
-import { TreeReactFlow, tree1, tree4, Error } from "@hackworthltd/primer-components";
+import { TreeReactFlow, Error } from "@hackworthltd/primer-components";
 import "@hackworthltd/primer-components/style.css";
 import { useParams } from "react-router-dom";
+import { useGetApiProgram } from "./primer-api";
 
 const App = (): JSX.Element => {
   const params = useParams();
   const session = params["sessionId"];
 
   if (session) {
-    return (
-      <div>
-        {"Session: " + session}
-        <TreeReactFlow
-          trees={[tree1, tree4]}
-          width={1000}
-          height={800}
-          nodeWidth={100}
-          nodeHeight={40}
-        ></TreeReactFlow>
-      </div>
-    );
+    const queryRes = useGetApiProgram({ session });
+
+    if (queryRes.isSuccess) {
+      const prog = queryRes.data;
+
+      const editableModules = prog.modules.filter((m) => m.editable);
+      if (editableModules.length > 1) {
+        return (
+          <Error
+            string={
+              "Multiple editable modules: " + JSON.stringify(editableModules)
+            }
+          />
+        );
+      }
+      const module = editableModules[0];
+
+      if (module) {
+        const trees = module.defs.flatMap((def) => def.term || []);
+        return (
+          <div>
+            <TreeReactFlow
+              trees={trees}
+              width={1500}
+              height={800}
+              nodeWidth={200}
+              nodeHeight={100}
+            ></TreeReactFlow>
+          </div>
+        );
+      } else {
+        return <Error string="No editable modules" />;
+      }
+    } else {
+      return (
+        <Error
+          string={
+            "Failed to get program from backend: " +
+            JSON.stringify(queryRes.error)
+          }
+        />
+      );
+    }
   } else {
     return (
       <Error string={"No session id parameter: " + JSON.stringify(params)} />
