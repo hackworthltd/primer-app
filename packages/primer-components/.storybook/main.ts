5403e152222091409146323693474379c572904b
Drew Hess <src@drewhess.com>
2022-07-30 13:18:17 +0100

    chore: Switch Storybook config to TypeScript.
    Note that there's some breakage here wrt. the vite-plugin-svgr plugin,
    but we can work around it with a `require`-style import. We also need
    to tell `ts-node` to use CommonJS when generating modules, or else
    `build-storybook` will complain about using imports outside of
    modules. (Sigh.) In any case, this should have no effect on generated
    code for the app, because it doesn't use `ts-node`.

--- packages/primer-components/.storybook/main.ts
+++ packages/primer-components/.storybook/main.ts
@@ -1,10 +1,14 @@
-const path = require("path");
+import type { StorybookViteConfig } from "@storybook/builder-vite";
 
-const svgr = require("vite-plugin-svgr").default;
+// Workaround for 'TypeError: (0 , vite_plugin_svgr_1.default) is not
+// a function' error. I hate Storybook.
+const svgr = require("vite-plugin-svgr");
+//import svgr from "vite-plugin-svgr";
 
-const tsconfigPaths = require("vite-tsconfig-paths").default;
+import tsconfigPaths from "vite-tsconfig-paths";
+import { mergeConfig } from "vite";
 
-module.exports = {
+const config: StorybookViteConfig = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   core: {
@@ -14,15 +18,12 @@ module.exports = {
   // storybook-builder-vite doesn't read the project's vite.config.js,
   // so we have to recreate the important bits here, mainly the
   // resolvers and the relevant plugins.
-  async viteFinal(config, { configType }) {
-    return {
-      ...config,
-      plugins: [...config.plugins, svgr(), tsconfigPaths()],
+  async viteFinal(config, _) {
+    return mergeConfig(config, {
+      plugins: [svgr(), tsconfigPaths()],
       // Don't write to node_modules, in case someday we can get it from Nix.
       cacheDir: "../.vite",
       build: {
-        ...config.build,
-
         // Workaround a dagre issue. See:
         //
         // https://github.com/vitejs/vite/issues/5759#issuecomment-1034461225
@@ -30,6 +31,8 @@ module.exports = {
           ignoreTryCatch: false,
         },
       },
-    };
+    });
   },
 };
+
+module.exports = config;
