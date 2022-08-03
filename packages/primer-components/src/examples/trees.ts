4597a561e2853c691f941cc53c054f3eb85f9d19
George Thomas <georgefsthomas@gmail.com>
2022-08-03 21:38:32 +0100

    fix: Update to work with new `Tree` type
    We commit to ReactFlow and drop our other tree renderers, rather than updating them to work with the new API.
    
    `packages/primer-types/src/Tree.ts` is a mess of copy-pasted type definitions from `primer-app`. But soon this will no longer be necessary as we'll fix our project structure / build system.
    
    This is enough to get the codebase to typecheck again, but is still a work-in-progress:
    - patterns not rendered
    - directions ignored i.e. right child treated as just another child - this means `match` expressions extend too far downward
    - definition name and type not shown
    - various color issues - in particular, many nodes are black

--- packages/primer-components/src/examples/trees.ts
+++ packages/primer-components/src/examples/trees.ts
@@ -3,80 +3,142 @@ import type { Tree, TreeInteractiveRender } from "@hackworthltd/primer-types";
 /*
 Some example trees.
 
-It's important that node IDs are unique across all these trees,
+It's important that node IDs are unique across all the numbered trees,
 as we'd like to render them simultaneously on one canvas.
 */
 
 export const tree1: Tree = {
-  nodeId: 100,
+  nodeId: "100",
   childTrees: [],
-  label: "EmptyHole",
+  flavor: "FlavorEmptyHole",
+  body: { tag: "NoBody" },
 };
 
 export const tree2: Tree = {
-  nodeId: 200,
-  childTrees: [{ nodeId: 201, childTrees: [], label: "Var x" }],
-  label: "Lam x",
+  nodeId: "200",
+  childTrees: [
+    {
+      nodeId: "201",
+      childTrees: [],
+      flavor: "FlavorLocalVar",
+      body: { tag: "TextBody", contents: "x" },
+    },
+  ],
+  flavor: "FlavorLam",
+  body: { tag: "TextBody", contents: "x" },
 };
 
 export const tree3: Tree = {
-  nodeId: 300,
-  childTrees: [{ nodeId: 301, childTrees: [], label: "Var y" }],
-  label: "Lam y",
+  nodeId: "300",
+  childTrees: [
+    {
+      nodeId: "301",
+      childTrees: [],
+      flavor: "FlavorLocalVar",
+      body: { tag: "TextBody", contents: "y" },
+    },
+  ],
+  flavor: "FlavorLam",
+  body: { tag: "TextBody", contents: "y" },
 };
 
 export const tree4: Tree = {
-  nodeId: 400,
+  nodeId: "400",
   childTrees: [
     {
-      nodeId: 401,
+      nodeId: "401",
       childTrees: [
         {
-          nodeId: 402,
+          nodeId: "402",
           childTrees: [
             {
-              nodeId: 403,
-              childTrees: [{ nodeId: 404, childTrees: [], label: "Var x" }],
-              label: "Lam x",
+              nodeId: "403",
+              childTrees: [
+                {
+                  nodeId: "404",
+                  childTrees: [],
+                  flavor: "FlavorLocalVar",
+                  body: { tag: "TextBody", contents: "x" },
+                },
+              ],
+              flavor: "FlavorLam",
+              body: { tag: "TextBody", contents: "x" },
             },
           ],
-          label: "LAM a",
+          flavor: "FlavorLAM",
+          body: { tag: "TextBody", contents: "a" },
         },
         {
-          nodeId: 405,
+          nodeId: "405",
           childTrees: [
             {
-              nodeId: 406,
+              nodeId: "406",
               childTrees: [
-                { nodeId: 407, childTrees: [], label: "TVar a" },
-                { nodeId: 408, childTrees: [], label: "TVar a" },
+                {
+                  nodeId: "407",
+                  childTrees: [],
+                  flavor: "FlavorTVar",
+                  body: { tag: "TextBody", contents: "a" },
+                },
+                {
+                  nodeId: "408",
+                  childTrees: [],
+                  flavor: "FlavorTVar",
+                  body: { tag: "TextBody", contents: "a" },
+                },
               ],
-              label: "TFun",
+              flavor: "FlavorTFun",
+              body: { tag: "NoBody" },
             },
           ],
-          label: "TForall",
+          flavor: "FlavorTForall",
+          body: { tag: "NoBody" },
         },
       ],
-      label: "Ann",
+      flavor: "FlavorAnn",
+      body: { tag: "NoBody" },
     },
-    { nodeId: 409, childTrees: [], label: "Con Unit" },
+    {
+      nodeId: "409",
+      childTrees: [],
+      flavor: "FlavorCon",
+      body: { tag: "TextBody", contents: "Unit" },
+    },
   ],
-  label: "App",
+  flavor: "FlavorApp",
+  body: { tag: "NoBody" },
 };
 
 export const tree5: TreeInteractiveRender = {
-  nodeId: 500,
+  nodeId: "500",
   childTrees: [
     {
-      nodeId: 501,
+      nodeId: "501",
       childTrees: [
-        { nodeId: 502, childTrees: [], label: "Var x" },
-        { nodeId: 503, childTrees: [], label: "EmptyHole" },
-        { nodeId: 506, childTrees: [], label: "EmptyHole" },
+        {
+          nodeId: "502",
+          childTrees: [],
+          flavor: "FlavorLocalVar",
+          body: { tag: "TextBody", contents: "x" },
+        },
+        {
+          nodeId: "503",
+          childTrees: [],
+          flavor: "FlavorEmptyHole",
+          body: { tag: "NoBody" },
+        },
+        {
+          nodeId: "506",
+          childTrees: [],
+          flavor: "FlavorEmptyHole",
+          body: { tag: "NoBody" },
+        },
       ],
-      label: "Case",
+      flavor: "FlavorCase",
+      body: { tag: "NoBody" },
     },
   ],
-  label: "Lam x",
+  flavor: "FlavorLam",
+  body: { tag: "TextBody", contents: "x" },
   onClick: (e) => console.log("Clicked: " + e),
 };
--- packages/primer-types/src/Tree.ts
+++ packages/primer-types/src/Tree.ts
@@ -6,24 +6,11 @@ import * as React from "react";
  * @interface Tree
  */
 export interface Tree {
-  /**
-   *
-   * @type {Array<Tree>}
-   * @memberof Tree
-   */
-  childTrees: Array<Tree>;
-  /**
-   *
-   * @type {number}
-   * @memberof Tree
-   */
-  nodeId: number;
-  /**
-   *
-   * @type {string}
-   * @memberof Tree
-   */
-  label: string;
+  body: NodeBody;
+  childTrees: Tree[];
+  nodeId: string;
+  rightChild?: Tree;
+  flavor: NodeFlavor;
 }
 
 /**
@@ -38,34 +25,76 @@ export interface Tree {
  * @interface TreeInteractiveRender
  */
 export interface TreeInteractiveRender {
-  /**
-   *
-   * @type {Array<Tree>}
-   * @memberof Tree
-   */
-  childTrees: Array<Tree>;
-  /**
-   *
-   * @type {number}
-   * @memberof Tree
-   */
-  nodeId: number;
-  /**
-   *
-   * @type {string}
-   * @memberof Tree
-   */
-  label: string;
-  /**
-   *
-   * @type {React.MouseEventHandler<unknown>}
-   * @memberof Tree
-   */
+  body: NodeBody;
+  childTrees: Tree[];
+  nodeId: string;
+  rightChild?: Tree;
+  flavor: NodeFlavor;
   onClick?: React.MouseEventHandler<unknown>;
-  /**
-   *
-   * @type {React.MouseEventHandler<unknown>}
-   * @memberof Tree
-   */
   onRightClick?: React.MouseEventHandler<unknown>;
 }
+
+export type NodeBody = NodeBodyOneOf | NodeBodyOneOfThree | NodeBodyOneOfFive;
+export type NodeBodyOneOf = {
+  contents: string;
+  tag: NodeBodyOneOfTag;
+};
+export type NodeBodyOneOfFive = {
+  tag: NodeBodyOneOfFiveTag;
+};
+export type NodeBodyOneOfFiveTag =
+  typeof NodeBodyOneOfFiveTag[keyof typeof NodeBodyOneOfFiveTag];
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const NodeBodyOneOfFiveTag = {
+  NoBody: "NoBody",
+} as const;
+export type NodeBodyOneOfTag =
+  typeof NodeBodyOneOfTag[keyof typeof NodeBodyOneOfTag];
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const NodeBodyOneOfTag = {
+  TextBody: "TextBody",
+} as const;
+export type NodeBodyOneOfThree = {
+  contents: Tree;
+  tag: NodeBodyOneOfThreeTag;
+};
+export type NodeBodyOneOfThreeTag =
+  typeof NodeBodyOneOfThreeTag[keyof typeof NodeBodyOneOfThreeTag];
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const NodeBodyOneOfThreeTag = {
+  BoxBody: "BoxBody",
+} as const;
+export type NodeFlavor = typeof NodeFlavor[keyof typeof NodeFlavor];
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const NodeFlavor = {
+  FlavorHole: "FlavorHole",
+  FlavorEmptyHole: "FlavorEmptyHole",
+  FlavorAnn: "FlavorAnn",
+  FlavorApp: "FlavorApp",
+  FlavorAPP: "FlavorAPP",
+  FlavorCon: "FlavorCon",
+  FlavorLam: "FlavorLam",
+  FlavorLAM: "FlavorLAM",
+  FlavorGlobalVar: "FlavorGlobalVar",
+  FlavorLocalVar: "FlavorLocalVar",
+  FlavorLet: "FlavorLet",
+  FlavorLetType: "FlavorLetType",
+  FlavorLetrec: "FlavorLetrec",
+  FlavorCase: "FlavorCase",
+  FlavorPrimCon: "FlavorPrimCon",
+  FlavorTEmptyHole: "FlavorTEmptyHole",
+  FlavorTHole: "FlavorTHole",
+  FlavorTCon: "FlavorTCon",
+  FlavorTFun: "FlavorTFun",
+  FlavorTVar: "FlavorTVar",
+  FlavorTApp: "FlavorTApp",
+  FlavorTForall: "FlavorTForall",
+  FlavorPattern: "FlavorPattern",
+  FlavorPatternCon: "FlavorPatternCon",
+  FlavorPatternBind: "FlavorPatternBind",
+  FlavorPatternApp: "FlavorPatternApp",
+} as const;
