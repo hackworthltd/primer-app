4df314988ba7f3d183689cff7bbe24b9582ea5f8
George Thomas <georgefsthomas@gmail.com>
2022-08-18 16:23:07 +0100

    refactor: Minor `layoutGraph` improvements

--- packages/primer-components/src/TreeReactFlow/layoutGraph.ts
+++ packages/primer-components/src/TreeReactFlow/layoutGraph.ts
@@ -9,18 +9,17 @@ export function layoutGraph(nodes: Node[], edges: Edge[]) {
   nodes.forEach((el) => {
     dagreGraph.setNode(el.id, { width: el.data.width, height: el.data.height });
   });
-
   edges.forEach((el) => {
     dagreGraph.setEdge(el.source, el.target);
   });
 
   layout(dagreGraph);
 
-  const layoutNodes = nodes.map((node) => {
+  return nodes.map((node) => {
     const nodeWithPosition = dagreGraph.node(node.id);
+
     node.targetPosition = Position.Top;
     node.sourcePosition = Position.Bottom;
-
     node.position = {
       x: nodeWithPosition.x - node.data.width / 2,
       y: nodeWithPosition.y - node.data.height / 2,
@@ -28,6 +27,4 @@ export function layoutGraph(nodes: Node[], edges: Edge[]) {
 
     return node;
   });
-
-  return layoutNodes;
 }
