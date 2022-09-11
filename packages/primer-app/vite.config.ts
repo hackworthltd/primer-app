a738951015704922914ad45ff79c22c1c2a92c92
Drew Hess <src@drewhess.com>
2022-09-11 13:32:45 +0100

    ci: Turn `eslint` warnings into errors.
    Upstream doesn't seem particularly interested in supporting this mode,
    but there's a workaround, which we implement in this commit:
    
    https://github.com/eslint/eslint/issues/2309#issuecomment-219828044
    
    This commit also fixes the remaining warnings in the current code
    base.

--- packages/primer-app/vite.config.ts
+++ packages/primer-app/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import checker from "vite-plugin-checker";
+import { checker } from "vite-plugin-checker";
 import PkgConfig from "vite-plugin-package-config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
