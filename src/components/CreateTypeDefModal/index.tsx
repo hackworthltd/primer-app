c516320460938d945c2fddedc522047e33ad032f
Drew Hess <src@drewhess.com>
2023-01-16 13:31:23 +0000

    refactor: Ensure typedef form's submit button is within the `<form>`.
    This ensures that pressing `Enter` from any field in the form will
    perform a submission, as is conventional.

--- src/components/CreateTypeDefModal/index.tsx
+++ src/components/CreateTypeDefModal/index.tsx
@@ -158,111 +158,113 @@ export const CreateTypeDefModal = (p: CreateTypeDefModalProps): JSX.Element => {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative overflow-hidden rounded-lg bg-white-primary px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-md bg-blue-secondary sm:mx-0 sm:h-10 sm:w-10">
-                    <SparklesIcon
-                      className="h-6 w-6 text-white-primary"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-lg font-medium leading-6 text-blue-primary"
-                    >
-                      Define a new type
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-blue-primary">
-                        Define your new type by giving it a name.
-                      </p>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-md bg-blue-secondary sm:mx-0 sm:h-10 sm:w-10">
+                      <SparklesIcon
+                        className="h-6 w-6 text-white-primary"
+                        aria-hidden="true"
+                      />
                     </div>
-                    <form
-                      onSubmit={handleSubmit(onSubmit)}
-                      className="mt-5 sm:flex sm:items-center"
-                    >
-                      <div className="w-full">
-                        <label htmlFor="typeName" className="sr-only">
-                          Type name
-                        </label>
-                        <input
-                          type="text"
-                          id="typeName"
-                          className="block w-full rounded-md border-grey-primary text-sm shadow-sm focus:ring-blue-secondary"
-                          aria-invalid={errors.typeName ? "true" : "false"}
-                          {...register("typeName")}
-                          placeholder="Type name"
-                        />
-                        {errors.typeName?.message && (
-                          <p className="mt-2 text-sm text-red-primary">
-                            <span role="alert">{errors.typeName.message}</span>
-                          </p>
-                        )}
-                        {fields.map((field, index) => {
-                          return (
-                            <div key={field.id} className="p-2">
-                              <div className="flex gap-2">
-                                <button
-                                  type="button"
-                                  onClick={() => remove(index)}
-                                >
-                                  <MinusIcon className={iconClasses} />
-                                </button>
-                                <input
-                                  type="text"
-                                  id={`constructorName${index}`}
-                                  className="block w-full rounded-md border-grey-primary text-sm shadow-sm focus:ring-blue-secondary"
-                                  aria-invalid={
-                                    errors.ctor?.[index]?.name
-                                      ? "true"
-                                      : "false"
-                                  }
-                                  {...register(`ctor.${index}.name` as const)}
-                                  placeholder="Value constructor name"
-                                />
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg font-medium leading-6 text-blue-primary"
+                      >
+                        Define a new type
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-blue-primary">
+                          Define your new type by giving it a name.
+                        </p>
+                      </div>
+                      <div className="mt-5 sm:flex sm:items-center">
+                        <div className="w-full">
+                          <label htmlFor="typeName" className="sr-only">
+                            Type name
+                          </label>
+                          <input
+                            type="text"
+                            id="typeName"
+                            className="block w-full rounded-md border-grey-primary text-sm shadow-sm focus:ring-blue-secondary"
+                            aria-invalid={errors.typeName ? "true" : "false"}
+                            {...register("typeName")}
+                            placeholder="Type name"
+                          />
+                          {errors.typeName?.message && (
+                            <p className="mt-2 text-sm text-red-primary">
+                              <span role="alert">
+                                {errors.typeName.message}
+                              </span>
+                            </p>
+                          )}
+                          {fields.map((field, index) => {
+                            return (
+                              <div key={field.id} className="p-2">
+                                <div className="flex gap-2">
+                                  <button
+                                    type="button"
+                                    onClick={() => remove(index)}
+                                  >
+                                    <MinusIcon className={iconClasses} />
+                                  </button>
+                                  <input
+                                    type="text"
+                                    id={`constructorName${index}`}
+                                    className="block w-full rounded-md border-grey-primary text-sm shadow-sm focus:ring-blue-secondary"
+                                    aria-invalid={
+                                      errors.ctor?.[index]?.name
+                                        ? "true"
+                                        : "false"
+                                    }
+                                    {...register(`ctor.${index}.name` as const)}
+                                    placeholder="Value constructor name"
+                                  />
+                                </div>
+                                {errors?.ctor?.[index]?.name && (
+                                  <p className="mt-2 text-sm text-red-primary">
+                                    <span role="alert">
+                                      {errors?.ctor?.[index]?.name?.message}
+                                    </span>
+                                  </p>
+                                )}
                               </div>
-                              {errors?.ctor?.[index]?.name && (
-                                <p className="mt-2 text-sm text-red-primary">
-                                  <span role="alert">
-                                    {errors?.ctor?.[index]?.name?.message}
-                                  </span>
-                                </p>
-                              )}
-                            </div>
-                          );
-                        })}
-                        <div>
-                          <button
-                            type="button"
-                            onClick={() =>
-                              append({
-                                name: "",
-                              })
-                            }
-                          >
-                            <PlusIcon className={iconClasses} />
-                          </button>
+                            );
+                          })}
+                          <div>
+                            <button
+                              type="button"
+                              onClick={() =>
+                                append({
+                                  name: "",
+                                })
+                              }
+                            >
+                              <PlusIcon className={iconClasses} />
+                            </button>
+                          </div>
                         </div>
                       </div>
-                    </form>
+                    </div>
                   </div>
-                </div>
-                <div className="mt-5 sm:flex sm:flex-row-reverse">
-                  <UIButton
-                    text="Create type"
-                    size="lg"
-                    appearance="primary"
-                    className="w-full justify-center sm:ml-3 sm:w-auto"
-                    onClick={handleSubmit(onSubmit)}
-                  />
-                  <UIButton
-                    text="Cancel"
-                    size="lg"
-                    appearance="plain"
-                    className="mt-3 w-full justify-center sm:mt-0 sm:w-auto"
-                    onClick={onCancel}
-                  />
-                </div>
+                  <div className="mt-5 sm:flex sm:flex-row-reverse">
+                    <UIButton
+                      buttonType="submit"
+                      text="Create type"
+                      size="lg"
+                      appearance="primary"
+                      className="w-full justify-center sm:ml-3 sm:w-auto"
+                      onClick={handleSubmit(onSubmit)}
+                    />
+                    <UIButton
+                      text="Cancel"
+                      size="lg"
+                      appearance="plain"
+                      className="mt-3 w-full justify-center sm:mt-0 sm:w-auto"
+                      onClick={onCancel}
+                    />
+                  </div>
+                </form>
               </Dialog.Panel>
             </Transition.Child>
           </div>
